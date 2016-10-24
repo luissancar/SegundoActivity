799e388a24413c566ae4530b8e48a21c7e14a95e
luis jose sanchez carrasco <luissancar@yahoo.es>
2016-10-24 19:17:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/luissancar/segundoactivity/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.luissancar.segundoactivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void segundo(View v) {
+
+        Intent i = new Intent(this, Main2Activity.class);
+        startActivity(i);
+    }
+
+}
